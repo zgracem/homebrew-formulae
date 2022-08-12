c08ec339bf2711fbf2ef37c68826c298e8189b48
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-08-12 14:52:38 -0600

    initial commit

---
+++ Formula/unrar.rb
@@ -0,0 +1,41 @@
+class Unrar < Formula
+  desc "Extract, view, and test RAR archives"
+  homepage "https://www.rarlab.com/"
+  url "https://www.rarlab.com/rar/unrarsrc-6.1.7.tar.gz"
+  sha256 "de75b6136958173fdfc530d38a0145b72342cf0d3842bf7bb120d336602d88ed"
+
+  livecheck do
+    url "https://www.rarlab.com/rar_add.htm"
+    regex(/href=.*?unrarsrc[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  def install
+    # upstream doesn't particularly care about their unix targets,
+    # so we do the dirty work of renaming their shared objects to
+    # dylibs for them.
+    inreplace "makefile", "libunrar.so", "libunrar.dylib"
+
+    system "make"
+    bin.install "unrar"
+
+    # Explicitly clean up for the library build to avoid an issue with an
+    # apparent implicit clean which confuses the dependencies.
+    system "make", "clean"
+    system "make", "lib"
+    lib.install shared_library("libunrar")
+  end
+
+  test do
+    contentpath = "directory/file.txt"
+    rarpath = testpath / "archive.rar"
+    data =  "UmFyIRoHAM+QcwAADQAAAAAAAACaCHQggDIACQAAAAkAAAADtPej1LZwZE" \
+            "QUMBIApIEAAGRpcmVjdG9yeVxmaWxlLnR4dEhvbWVicmV3CsQ9ewBABwA="
+
+    rarpath.write data.unpack1("m")
+    assert_equal contentpath, `#{bin}/unrar lb #{rarpath}`.strip
+    assert_equal 0, $CHILD_STATUS.exitstatus
+
+    system "#{bin}/unrar", "x", rarpath, testpath
+    assert_equal "Homebrew\n", (testpath / contentpath).read
+  end
+end
---
+++ README.md
@@ -0,0 +1,8 @@
+# homebrew-formulae
+
+My own personal custom Homebrew formulae.
+
+## `unrar`
+
+`unrar` was [removed](https://github.com/Homebrew/homebrew-core/pull/66609) from
+Homebrew in December 2020 because it's not open source software.
