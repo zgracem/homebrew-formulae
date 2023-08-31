3a77167fc42e9025bff3aa06dc023b2b541e736e
Amy Grace <zgracem@users.noreply.github.com>
2023-08-31 09:57:50 -0600

    lockscreen: minor edits for style

--- Casks/lockscreen.rb
+++ Casks/lockscreen.rb
@@ -1,19 +1,18 @@
 cask "lockscreen" do
-  version "1.0.0b1"
   sha256 "893cf104aebb4af393f4751f632d279f636c027c58d6864faf80e92a83201030"
 
-  url "https://github.com/gaomd/lock-screen-app/releases/download/v1.0.0-beta.1/Lock-Screen-#{version}.dmg"
+  url "https://github.com/gaomd/lock-screen-app/releases/download/v1.0.0-beta.1/Lock-Screen-1.0.0b1.dmg"
   name "Lock Screen"
   desc "The missing Lock Screen app for Spotlight of macOS"
   homepage "https://github.com/gaomd/lock-screen-app"
-
-  auto_updates false
-  depends_on macos: ">= :high_sierra"
 
   livecheck do
     url :url
     strategy :github_latest
   end
+
+  auto_updates false
+  depends_on macos: ">= :high_sierra"
 
   app "Lock Screen.app"
 
