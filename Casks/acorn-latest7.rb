600c79aba944f325e0cda55d142716155cc85a42
Amy Grace <zgracem@users.noreply.github.com>
2023-12-18 10:49:32 -0700

    acorn-latest7: small changes

--- Casks/acorn-latest7.rb
+++ Casks/acorn-latest7.rb
@@ -10,7 +10,7 @@ cask "acorn-latest7" do
   livecheck do
     url "https://flyingmeat.com/download/latest/"
     strategy :page_match do |page|
-      page.scan(%r{(?<=Acorn 7 Preview\.zip</a> \()(\d+(?:\.\d+)*(?:[ab]\d+) \(\d+\))}i)
+      page.scan(/(?<=Acorn 7 Preview\.zip<\/a> \()(\d+(?:\.\d+)*(?:[ab]\d+) \(\d+\))/i)
           .map { |match| match&.first }
     end
   end
@@ -21,15 +21,13 @@ cask "acorn-latest7" do
   app "Acorn.app"
 
   zap trash: [
-    "~/Library/Application Scripts/com.flyingmeat.Acorn7.Acornappex",
-    "~/Library/Application Scripts/com.flyingmeat.Acorn7.editinacorn",
     "~/Library/Application Support/Acorn",
-    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.flyingmeat.acorn7.sfl3",
-    "~/Library/Caches/com.flyingmeat.Acorn7",
-    "~/Library/Containers/com.flyingmeat.Acorn7.Acornappex",
-    "~/Library/Containers/com.flyingmeat.Acorn7.editinacorn",
-    "~/Library/HTTPStorages/com.flyingmeat.Acorn7",
-    "~/Library/Preferences/com.flyingmeat.Acorn7.plist",
-    "~/Library/Saved Application State/com.flyingmeat.Acorn7.savedState",
+    "~/Library/Caches/com.flyingmeat.Acorn#{version.major}",
+    "~/Library/Preferences/com.flyingmeat.Acorn#{version.major}.plist",
+    "~/Library/Saved Application State/com.flyingmeat.Acorn#{version.major}.savedState",
+    "~/Library/Application Scripts/com.flyingmeat.Acorn#{version.major}.*",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.flyingmeat.acorn#{version.major}.sfl3",
+    "~/Library/Containers/com.flyingmeat.Acorn#{version.major}.*",
+    "~/Library/HTTPStorages/com.flyingmeat.Acorn#{version.major}",
   ]
 end
