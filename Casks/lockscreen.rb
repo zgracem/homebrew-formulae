afeb128e2a96656f274039907f2ece34eb77e7e0
Z. Grace Moreau <zgracem@users.noreply.github.com>
2025-11-10 11:31:14 -0700

    lockscreen: fix

--- Casks/lockscreen.rb
+++ Casks/lockscreen.rb
@@ -1,9 +1,8 @@
 cask "lockscreen" do
-  sha256 "893cf104aebb4af393f4751f632d279f636c027c58d6864faf80e92a83201030"
+  sha256 :no_check
 
   url "https://github.com/gaomd/lock-screen-app/releases/download/v1.0.0-beta.1/Lock-Screen-1.0.0b1.dmg"
   version "1.0.0b1"
-  sha256 :no_check
   name "Lock Screen"
   desc "The missing Lock Screen app for Spotlight of macOS"
   homepage "https://github.com/gaomd/lock-screen-app"
